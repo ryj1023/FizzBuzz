150328d54f1611a0236a05ebbace2785e4c9430f
Ryan Johnson <ryj1023@yahoo.com>
2016-05-07 10:58:42 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+
+
+for (var i = 1; i <= 100; i++)
+{
+var a = i%15 == 0;
+var b = i%3 == 0;
+var c = i%5 == 0;
+
+	if (a)
+	{	
+	$('#list').append('<li class="numbers"> <span>' + "FizzBuzz" + '</span> </li>');
+	}
+	else if (b)
+	{
+	$('#list').append('<li class="numbers"> <span>' + "Fizz" + '</span> </li>');
+	}
+
+	else if (c)
+	{
+	$('#list').append('<li class="numbers"> <span>' + "Buzz" + '</span> </li>');
+	}
+
+	
+
+	else 
+		
+	$('#list').append('<li class="numbers"> <span>' + i + '</span> </li>');	
+
+	
+ 
+
+}
+
+
+
+	
+
+//$('#list').append('<li class="numbers"> <span>' + ryan + '</span> </li>');
+
+
+});
